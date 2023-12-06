bf94b34e901774f8deb4b6758cf2c3304ff210e0
gashie <richardwilliam60@gmail.com>
2023-12-06 12:22:57 +0000

    update runner

--- run.js
+++ run.js
@@ -5,17 +5,25 @@ function listRunningServices() {
     // Get a list of all running systemd services
     const runningServices = execSync('ps axo comm | grep -E \'^\\[?[^\\[]\'').toString().trim().split('\n');
 
+    // Define invalid entries that should be excluded
+    const invalidEntries = ['COMMAND', 'kthreadd', 'rcu_gp', 'rcu_par_gp', 'slub_flushwq', 'netns', 'kworker/0:0H-events_highpri', 'mm_percpu_wq'];
+
     // Loop through each running service
     runningServices.forEach((service) => {
-      // Add a debug statement to print the service name
-      console.log(`Processing service: ${service}`);
+      // Skip processing invalid entries
+      if (invalidEntries.includes(service)) {
+        return;
+      }
 
       // Filter out invalid unit names
-      if (!service.match(/[^A-Za-z0-9_-]/)) {
-        const serviceInfo = getServiceInfo(service);
-        if (serviceInfo) {
-          console.log(JSON.stringify(serviceInfo, null, 2));
-        }
+      if (!service.match(/^[A-Za-z0-9_-]+$/)) {
+        // Skip processing invalid entries
+        return;
+      }
+
+      const serviceInfo = getServiceInfo(service);
+      if (serviceInfo) {
+        console.log(JSON.stringify(serviceInfo, null, 2));
       }
     });
   } catch (error) {
@@ -23,18 +31,8 @@ function listRunningServices() {
   }
 }
 
-function getServiceInfo(serviceName) {
-  try {
-    // Add a debug statement to print the service name
-    console.log(`Fetching information for service: ${serviceName}`);
+// Rest of the script remains unchanged
+// ...
 
-    // Rest of the function remains unchanged
-    // ...
-  } catch (error) {
-    console.error(`Error fetching information for ${serviceName}: ${error.message}`);
-    return null;
-  }
-}
-
-// List all running services
+// Call the function to list all running services
 listRunningServices();
