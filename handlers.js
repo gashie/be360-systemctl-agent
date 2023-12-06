c4c6511ec252b17252ee87a5ace81c561097dd9c
gashie <richardwilliam60@gmail.com>
2023-12-06 10:41:44 +0000

    change response data

--- handlers.js
+++ handlers.js
@@ -77,22 +77,50 @@ exports.getServiceStatus = function (request, response) {
 	});
 };
 exports.listService = function (request, response) {
-    const { exec } = require('child_process');
-  const cmd = 'systemctl list-units --no-legend';
-  exec(cmd, (error, stdout, stderr) => {
-    if (error) {
-      console.error('Error listing units');
-      console.error(stderr);
-    } else {
-      const lines = stdout.trim().split('\n');
-      const unitsList = [];
-
-      lines.forEach((line) => {
-        const [unit, load, active, sub, description] = line.split(/\s+/);
-        unitsList.push({ unit, load, active, sub, description });
-      });
-
-      response.send(JSON.stringify(unitsList, null, 2))
-    }
-  });
+	const cmd = 'systemctl list-units --no-legend';
+	exec(cmd, (error, stdout, stderr) => {
+	  if (error) {
+		console.error('Error listing units');
+		console.error(stderr);
+	  } else {
+		const lines = stdout.trim().split('\n');
+		const unitsList = [];
+  
+		lines.forEach((line) => {
+		  const [unit, load, active, sub, description] = line.split(/\s+/);
+  
+		  const detailedInfoCmd = `systemctl show ${unit} --no-pager --property=LoadState,ActiveState,Description,SubState,UnitFileState,ActiveEnterTimestamp,MainPID`;
+		  exec(detailedInfoCmd, (detailError, detailStdout, detailStderr) => {
+			if (detailError) {
+			  console.error(`Error getting detailed info for ${unit}`);
+			  console.error(detailStderr);
+			} else {
+			  const [loaded, activeState, desc, subState, unitFileState, activeEnterTimestamp, mainPID] = detailStdout.trim().split(/\n/);
+  
+			  const unitInfo = {
+				name: unit,
+				description: desc.split('=')[1],
+				loaded: loaded.split('=')[1] === 'loaded',
+				file: unitFileState.split('=')[1],
+				startup: activeState.split('=')[1],
+				props: {
+				  preset: subState.split('=')[1],
+				},
+				active: activeState.split('=')[1],
+				started: new Date(parseInt(activeEnterTimestamp.split('=')[1]) * 1000).toISOString(),
+				pid: mainPID.split('=')[1],
+				raw: detailStdout.trim(),
+			  };
+  
+			  unitsList.push(unitInfo);
+			}
+  
+			// Print the JSON result when all units are processed
+			if (unitsList.length === lines.length) {
+			  response.json(JSON.stringify(unitsList, null, 2))
+			}
+		  });
+		});
+	  }
+	});
 };
