f89655133b80ddff76b50b0ca96d0a0244dc7374
gashie <richardwilliam60@gmail.com>
2023-12-06 12:03:29 +0000

    updated run to preset

--- run.js
+++ run.js
@@ -1,23 +1,55 @@
+const { execSync } = require('child_process');
 
-function listUnits() {
-    const { exec } = require('child_process');
-    const cmd = 'systemctl list-units --no-legend';
-    exec(cmd, (error, stdout, stderr) => {
-      if (error) {
-        console.error('Error listing units');
-        console.error(stderr);
-      } else {
-        const lines = stdout.trim().split('\n');
-        const unitsList = [];
-  
-        lines.forEach((line) => {
-          const [unit, load, active, sub, description] = line.split(/\s+/);
-          unitsList.push({ unit, load, active, sub, description });
-        });
-  
-        console.log(JSON.stringify(unitsList, null, 2));
-      }
-    });
-  }
-  
-  listUnits();
+function listRunningServices() {
+  // Get a list of all running services
+  const runningServices = execSync('ps axo comm | grep -E \'^\\[?[^\\[]\'').toString().trim().split('\n');
+
+  // Loop through each running service
+  runningServices.forEach((service) => {
+    const serviceInfo = getServiceInfo(service);
+    console.log(JSON.stringify(serviceInfo, null, 2));
+  });
+}
+
+function getServiceInfo(serviceName) {
+  // Get basic information
+  const name = execSync(`systemctl show -p Id -p Description -p LoadState -p FragmentPath --value ${serviceName}`).toString().trim();
+
+  // Get detailed information
+  const details = execSync(`systemctl show --no-page --value ${serviceName}`).toString().trim();
+
+  // Extract specific details
+  const loaded = execSync(`systemctl show --no-page --value --property=LoadState ${serviceName}`).toString().trim() === 'loaded';
+  const file = execSync(`systemctl show --no-page --value --property=FragmentPath ${serviceName}`).toString().trim();
+  const startup = execSync(`systemctl is-enabled ${serviceName}`).toString().trim().toLowerCase();
+  const preset = execSync(`systemctl show --no-page --value --property=SubState ${serviceName}`).toString().trim();
+
+  // Get status, started time, and PID
+  const status = execSync(`systemctl is-active ${serviceName}`).toString().trim();
+  const started = execSync(`systemctl show --no-page --value --property=ActiveEnterTimestamp ${serviceName}`).toString().trim();
+  const pid = execSync(`systemctl show --no-page --value --property=MainPID ${serviceName}`).toString().trim();
+
+  // Get raw message
+  const raw = execSync(`systemctl show --no-page --value ${serviceName}`).toString().trim();
+
+  // Format the output
+  const output = {
+    name,
+    description: name.split(' ').slice(1).join(' '),
+    loaded,
+    file,
+    startup,
+    props: {
+      preset,
+    },
+    status,
+    started: started ? new Date(parseInt(started) * 1000).toISOString() : null,
+    pid,
+    raw,
+  };
+
+  return output;
+}
+
+// List all running services
+listRunningServices();
