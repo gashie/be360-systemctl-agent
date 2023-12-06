34b3660639150bda5af381533e09c1d1f7f78d92
gashie <richardwilliam60@gmail.com>
2023-12-06 11:26:55 +0000

    add fields

--- run.js
+++ run.js
@@ -2,21 +2,29 @@ const sysctlx = require('sysctlx');
 
 async function listRunningServices() {
   try {
-    // Run systemctl command to list all running services
+    // Run systemctl command to list all running services in plain text
     const result = await sysctlx.systemctl('list-units', '--type=service', '--state=running', '--plain', '--all', '--no-pager');
 
-    try {
-      // Parse the JSON result
-      const services =result;
+    // Split the output into lines
+    const lines = result.split('\n');
 
-      // Display the list of running services
-      console.log('Running Services:',services);
-    //   services.forEach(service => {
-    //     console.log(`- ${service[0]}`);
-    //   });
-    } catch (jsonError) {
-      console.error('Error parsing JSON:', jsonError);
-      console.log('Raw Output:', result);
+    // Display the list of running services with additional details
+    console.log('Running Services:');
+    for (const line of lines) {
+      // Skip empty lines
+      if (!line.trim()) continue;
+
+      // Split the line into columns
+      const columns = line.split(/\s+/);
+
+      // Extract relevant information
+      const unit = columns[0];
+      const description = columns.slice(4).join(' ');
+
+      // Display information
+      console.log(`Unit: ${unit}`);
+      console.log(`  Description: ${description}`);
+      console.log('---');
     }
   } catch (error) {
     // Ignore errors related to init.scope
