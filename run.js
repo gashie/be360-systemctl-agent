c94ef33920fd8877db7c45de96a623b749322931
gashie <richardwilliam60@gmail.com>
2023-12-06 12:09:57 +0000

    fix again

--- run.js
+++ run.js
@@ -1,5 +1,22 @@
 const { execSync } = require('child_process');
 
+function listRunningServices() {
+  try {
+    // Get a list of all running systemd services
+    const runningServices = execSync('ps axo comm | grep -E \'^\\[?[^\\[]\'').toString().trim().split('\n');
+
+    // Loop through each running service
+    runningServices.forEach((service) => {
+      const serviceInfo = getServiceInfo(service);
+      if (serviceInfo) {
+        console.log(JSON.stringify(serviceInfo, null, 2));
+      }
+    });
+  } catch (error) {
+    console.error(`Error listing running services: ${error.message}`);
+  }
+}
+
 function getServiceInfo(serviceName) {
   try {
     // Get basic information
@@ -47,20 +64,3 @@ function getServiceInfo(serviceName) {
 
 // List all running services
 listRunningServices();
-
-function listRunningServices() {
-  try {
-    // Get a list of all running services
-    const runningServices = execSync('ps axo comm | grep -E \'^\\[?[^\\[]\'').toString().trim().split('\n');
-
-    // Loop through each running service
-    runningServices.forEach((service) => {
-      const serviceInfo = getServiceInfo(service);
-      if (serviceInfo) {
-        console.log(JSON.stringify(serviceInfo, null, 2));
-      }
-    });
-  } catch (error) {
-    console.error(`Error listing running services: ${error.message}`);
-  }
-}
