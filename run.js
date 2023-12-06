fd606895f7f5af57da702f6af0d19a0adb4a41a3
gashie <richardwilliam60@gmail.com>
2023-12-06 11:18:21 +0000

    fix run

--- run.js
+++ run.js
@@ -1,50 +1,30 @@
-const { execSync } = require('child_process');
+const sysctlx = require('sysctlx');
 
-function getServiceInfo(serviceName) {
-  // Get basic information
-  const name = execSync(`systemctl show -p Id -p Description -p LoadState -p FragmentPath --value ${serviceName}`).toString().trim();
+async function listRunningServices() {
+  try {
+    // Run systemctl command to list all running services
+    const result = await sysctlx.systemctl('list-units', '--type=service', '--state=running', '--plain', '--all', '--no-pager', '--json');
 
-  // Get detailed information
-  const details = execSync(`systemctl show --no-page --value ${serviceName}`).toString().trim();
+    try {
+      // Parse the JSON result
+      const services = JSON.parse(result);
 
-  // Extract specific details
-  const loaded = execSync(`systemctl show --no-page --value --property=LoadState ${serviceName}`).toString().trim() === 'loaded';
-  const file = execSync(`systemctl show --no-page --value --property=FragmentPath ${serviceName}`).toString().trim();
-  const startup = execSync(`systemctl is-enabled ${serviceName}`).toString().trim().toLowerCase();
-  const preset = execSync(`systemctl show --no-page --value --property=SubState ${serviceName}`).toString().trim();
-
-  // Get status, started time, and PID
-  const status = execSync(`systemctl is-active ${serviceName}`).toString().trim();
-  const started = execSync(`systemctl show --no-page --value --property=ActiveEnterTimestamp ${serviceName}`).toString().trim();
-  const pid = execSync(`systemctl show --no-page --value --property=MainPID ${serviceName}`).toString().trim();
-
-  // Get raw message
-  const raw = execSync(`systemctl show --no-page --value ${serviceName}`).toString().trim();
-
-  // Format the output
-  const output = {
-    name,
-    description: name.split(' ').slice(1).join(' '),
-    loaded,
-    file,
-    startup,
-    props: {
-      preset,
-    },
-    status,
-    started: started,
-    pid,
-    raw,
-  };
-
-  return output;
+      // Display the list of running services
+      console.log('Running Services:');
+      services.forEach(service => {
+        console.log(`- ${service[0]}`);
+      });
+    } catch (jsonError) {
+      console.error('Error parsing JSON:', jsonError);
+      console.log('Raw Output:', result);
+    }
+  } catch (error) {
+    // Ignore errors related to init.scope
+    if (!error.stderr.includes('Failed to get unit file state for init.scope: No such file or directory')) {
+      console.error('Error running systemctl command:', error);
+    }
+  }
 }
 
-// Get a list of all running services
-const runningServices = execSync('systemctl list-units --no-legend --state=running --plain --full --no-pager | awk \'{print $1}\'').toString().trim().split('\n');
-
-// Loop through each running service
-runningServices.forEach((service) => {
-  const serviceInfo = getServiceInfo(service);
-  console.log(JSON.stringify(serviceInfo, null, 2));
-});
+// Call the function to list running services
+listRunningServices();
